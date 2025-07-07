1f966005f0409d08ea0cfeeb7374f02d4c274673
Khawar <hayatkhawar219@gmail.com>
2025-07-07 21:03:42 +0500

    add prefix before class name and other related code

--- templates/template-1.php
+++ templates/template-1.php
@@ -1,27 +1,27 @@
-<div class="glasses-sale-banner">
-        <div class="glasses-sale-banner-container">
-            <div class="glasses-brand-info">
-                <div class="glasses-ad-container">
-                    <h1 class="glasses-title">{{title}}</h1>
-                    <h2 class="glasses-sub-title">{{subtitle}}</h2>
-                    <p class="glasses-tagline">{{description}}</p>
-                    <button class="glasses-shop-button" data-href="#">{{shop now}}</button>
+<div class="wpgs-glasses-sale-banner">
+        <div class="wpgs-glasses-sale-banner-container">
+            <div class="wpgs-glasses-brand-info">
+                <div class="wpgs-glasses-ad-container">
+                    <h1 class="wpgs-glasses-title">{{title}}</h1>
+                    <h2 class="wpgs-glasses-sub-title">{{subtitle}}</h2>
+                    <p class="wpgs-glasses-tagline">{{description}}</p>
+                    <button class="wpgs-glasses-shop-button" data-href="#">{{shop now}}</button>
                 </div>
             </div>
-            <div class="glasses-offer-banner">
-                <div class="glasses-offer-text">
-                    <div class="glasses-discount-line">
-                        <span class="glasses-big-number">50</span>
+            <div class="wpgs-glasses-offer-banner">
+                <div class="wpgs-glasses-offer-text">
+                    <div class="wpgs-glasses-discount-line">
+                        <span class="wpgs-glasses-big-number">50</span>
                     </div>
-                    <div class="glasses-percent-off">
-                        <span class="glasses-percent">%</span>
-                        <span class="glasses-off">OFF</span>
+                    <div class="wpgs-glasses-percent-off">
+                        <span class="wpgs-glasses-percent">%</span>
+                        <span class="wpgs-glasses-off">OFF</span>
                     </div>
                 </div>
-                   <div id="{{countdown_id}}" class="wbgacountdown glasses-countdown" data-end-time="{{end_time}}"></div> 
+                   <div id="{{countdown_id}}" class="wbgscountdown wpgs-glasses-countdown" data-end-time="{{end_time}}"></div> 
                   </div>
             </div>
-            <div class="glasses-sale-image">
+            <div class="wpgs-glasses-sale-image">
                 <img src="{{image_url}}" alt="{{title}}">
             </div>
        </div>
