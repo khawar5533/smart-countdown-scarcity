95e28d18fe0b8692dab1901b46dad5ceb04017f2
yousufakram <yousufakram5154@gmail.com>
2025-07-11 03:11:01 +0500

    banner responsiveness issue have been resolved

--- templates/template-1.php
+++ templates/template-1.php
@@ -1,27 +1,27 @@
 <div class="wbgs-glasses-sale-banner">
-        <div class="wbgs-glasses-sale-banner-container">
-            <div class="wbgs-glasses-brand-info">
-                <div class="wbgs-glasses-ad-container">
-                    <h1 class="wbgs-glasses-title">{{title}}</h1>
-                    <h2 class="wbgs-glasses-sub-title">{{subtitle}}</h2>
-                    <p class="wbgs-glasses-tagline">{{description}}</p>
-                    <button class="wbgs-glasses-shop-button" data-href="{{url}}">{{shop now}}</button>
+    <div class="wbgs-glasses-sale-banner-container">
+        <div class="wbgs-glasses-brand-info">
+            <div class="wbgs-glasses-ad-container">
+                <h1 class="wbgs-glasses-title">{{title}}</h1>
+                <h2 class="wbgs-glasses-sub-title">{{subtitle}}</h2>
+                <p class="wbgs-glasses-tagline">{{description}}</p>
+                <button class="wbgs-glasses-shop-button" data-href="{{url}}">{{shop now}}</button>
+            </div>
+        </div>
+        <div class="wbgs-glasses-offer-banner">
+            <div class="wbgs-glasses-offer-text">
+                <div class="wbgs-glasses-discount-line">
+                    <span class="wbgs-glasses-big-number">{{off}}</span>
+                </div>
+                <div class="wbgs-glasses-percent-off">
+                    <span class="wbgs-glasses-percent">{{percent}}</span>
+                    <span class="wbgs-glasses-off">{{discounttitle}}</span>
                 </div>
             </div>
-            <div class="wbgs-glasses-offer-banner">
-                <div class="wbgs-glasses-offer-text">
-                    <div class="wbgs-glasses-discount-line">
-                        <span class="wbgs-glasses-big-number">{{off}}</span>
-                    </div>
-                    <div class="wbgs-glasses-percent-off">
-                        <span class="wbgs-glasses-percent">{{percent}}</span>
-                        <span class="wbgs-glasses-off">{{discounttitle}}</span>
-                    </div>
-                </div>
-                   <div id="{{countdown_id}}" class="wbgscountdown wbgs-glasses-countdown" data-end-time="{{end_time}}"></div> 
-                  </div>
-            </div>
-            <div class="wbgs-glasses-sale-image">
-                <img src="{{image_url}}" alt="{{title}}">
-            </div>
-       </div>
+            <div id="{{countdown_id}}" class="wbgscountdown wbgs-glasses-countdown" data-end-time="{{end_time}}"></div>
+        </div>
+        <div class="wbgs-glasses-sale-image">
+            <img src="{{image_url}}" alt="{{title}}">
+        </div>
+    </div>
+</div>
