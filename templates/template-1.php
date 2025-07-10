68127abd7f1f3e2eb55da2e8547171949f5ae8df
Khawar <hayatkhawar219@gmail.com>
2025-07-11 01:17:11 +0500

    update the class prefix

--- templates/template-1.php
+++ templates/template-1.php
@@ -1,27 +1,27 @@
-<div class="wpgs-glasses-sale-banner">
-        <div class="wpgs-glasses-sale-banner-container">
-            <div class="wpgs-glasses-brand-info">
-                <div class="wpgs-glasses-ad-container">
-                    <h1 class="wpgs-glasses-title">{{title}}</h1>
-                    <h2 class="wpgs-glasses-sub-title">{{subtitle}}</h2>
-                    <p class="wpgs-glasses-tagline">{{description}}</p>
-                    <button class="wpgs-glasses-shop-button" data-href="{{url}}">{{shop now}}</button>
+<div class="wbgs-glasses-sale-banner">
+        <div class="wbgs-glasses-sale-banner-container">
+            <div class="wbgs-glasses-brand-info">
+                <div class="wbgs-glasses-ad-container">
+                    <h1 class="wbgs-glasses-title">{{title}}</h1>
+                    <h2 class="wbgs-glasses-sub-title">{{subtitle}}</h2>
+                    <p class="wbgs-glasses-tagline">{{description}}</p>
+                    <button class="wbgs-glasses-shop-button" data-href="{{url}}">{{shop now}}</button>
                 </div>
             </div>
-            <div class="wpgs-glasses-offer-banner">
-                <div class="wpgs-glasses-offer-text">
-                    <div class="wpgs-glasses-discount-line">
-                        <span class="wpgs-glasses-big-number">{{off}}</span>
+            <div class="wbgs-glasses-offer-banner">
+                <div class="wbgs-glasses-offer-text">
+                    <div class="wbgs-glasses-discount-line">
+                        <span class="wbgs-glasses-big-number">{{off}}</span>
                     </div>
-                    <div class="wpgs-glasses-percent-off">
-                        <span class="wpgs-glasses-percent">{{percent}}</span>
-                        <span class="wpgs-glasses-off">{{discounttitle}}</span>
+                    <div class="wbgs-glasses-percent-off">
+                        <span class="wbgs-glasses-percent">{{percent}}</span>
+                        <span class="wbgs-glasses-off">{{discounttitle}}</span>
                     </div>
                 </div>
-                   <div id="{{countdown_id}}" class="wbgscountdown wpgs-glasses-countdown" data-end-time="{{end_time}}"></div> 
+                   <div id="{{countdown_id}}" class="wbgscountdown wbgs-glasses-countdown" data-end-time="{{end_time}}"></div> 
                   </div>
             </div>
-            <div class="wpgs-glasses-sale-image">
+            <div class="wbgs-glasses-sale-image">
                 <img src="{{image_url}}" alt="{{title}}">
             </div>
        </div>
